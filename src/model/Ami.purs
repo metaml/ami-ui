1b096d36201d4b50959b38b4668e3543c2b5b69d
metaml <metaml@gmail.com>
2024-09-11 00:41:08 -0700

    removed redundant line

--- src/model/Ami.purs
+++ src/model/Ami.purs
@@ -1,20 +1,13 @@
 module Ami where
 
 import Prelude
-import Control.Bind ((=<<))
 import Data.Argonaut.Core (stringify)
-import Data.Argonaut.Decode (parseJson, decodeJson)
-import Data.Argonaut.Decode.Error (JsonDecodeError)
+import Data.Argonaut.Decode (decodeJson)
 import Data.Argonaut.Encode (encodeJson)
-import Data.Either
-import Data.Functor
-import Effect.Aff (Aff, attempt)
-import Effect.Console (log, logShow)
+import Data.Either (Either(..))
+import Effect.Aff (Aff)
 import Fetch as F
-import Fetch.Argonaut.Json (fromJson)
-import Foreign
-import Halogen as H
-import Undefined
+import Foreign (unsafeFromForeign)
 
 type Message  = { content :: String, role :: String }
 type Request  = { messages :: Array Message, stream :: Boolean }
@@ -23,13 +16,13 @@ type Response = { messages :: Array Message, friend :: String }
 talk :: Request -> Aff Response
 talk req = do
   let url = "http://localhost:8000/talk"
-  r <- F.fetch url { method: F.POST
-                   , body: stringify (encodeJson req)
-                   , headers: { "Content-Type": "application/json" }
-                   }
-  json <- r.json
-  let js = (unsafeFromForeign json)
-      res = decodeJson js :: Either _ Response
-  case res of
-    Left e  -> pure { messages: [], friend: "Courtney" }
+  res <- F.fetch url { method: F.POST
+                     , body: stringify (encodeJson req)
+                     , headers: { "Content-Type": "application/json" }
+                     }
+  json <- res.json
+  let js = unsafeFromForeign json
+      res' = decodeJson js :: Either _ Response
+  case res' of
+    Left _  -> pure { messages: [], friend: "Courtney" }
     Right r -> pure r
